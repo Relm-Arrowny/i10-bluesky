bb4e99ff42fa5d71a3f68c5c0e4097d7c31319a0
Raymond Fan <wvx67826@ws545.diamond.ac.uk>
2024-03-05 15:43:02 +0000

    correct branch name

--- src/i10_bluesky/devices/rasor/detectorSlits.py
+++ src/i10_bluesky/devices/rasor/detectorSlits.py
@@ -1,12 +1,3 @@
-"""
-from ophyd import Component, Device, EpicsMotor
-
-
-class DetectorSlits(Device):
-    dsu = Component(EpicsMotor, "-01:TRANS")
-    dsd = Component(EpicsMotor, "-02:TRANS")
-"""
-
 from ophyd_async.core import Device
 from ophyd_async.epics.motion.motor import Motor
 
@@ -17,19 +8,3 @@ class DetectorSlits(Device):
         self.dsu = Motor(prefix + "-01:TRANS")
         self.dsd = Motor(prefix + "-02:TRANS")
         super().__init__(name=name)
-
-
-"""
-class Slits(StandardReadable):
-    # basic slits class
-
-    def __init__(self, prefix: str, suffix: str, name: str = "") -> None:
-        # Define some signals
-        self.value = epics_signal_r(float, prefix + suffix)
-        # Set name and signals for read() and read_configuration()
-        self.set_readable_signals(
-            read=[self.value],
-            # config=[self.mode],
-        )
-        super().__init__(name=name)
-"""
