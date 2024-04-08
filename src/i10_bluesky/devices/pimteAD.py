7527768f837aca488ba104ef48f7886b63b94f04
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-04-08 14:15:27 +0000

    added tigger and prepare plan

--- src/i10_bluesky/devices/pimteAD.py
+++ src/i10_bluesky/devices/pimteAD.py
@@ -2,9 +2,8 @@ from typing import Sequence
 
 from bluesky.protocols import Hints
 from ophyd_async.core import DirectoryProvider, SignalR, StandardDetector
-from ophyd_async.core.async_status import AsyncStatus
 from ophyd_async.epics.areadetector.drivers import ADBaseShapeProvider
-from ophyd_async.epics.areadetector.writers import HDFWriter, NDFileHDF
+from ophyd_async.epics.areadetector.writers import HDFWriter, NDFileHDF, NDPluginStats
 
 from i10_bluesky.epics.drivers.pimte1_driver import Pimte1Driver
 from i10_bluesky.epics.pimte_controller import PimteController
@@ -24,7 +23,7 @@ class HDFStatsPimte(StandardDetector):
     ):
         self.drv = Pimte1Driver(prefix + "CAM:")
         self.hdf = NDFileHDF(prefix + "HDF5:")
-        # self.stats = NDPluginStats(prefix + "STAT:")
+        self.stats = NDPluginStats(prefix + "STAT:")
         # taken from i22 but this does nothing atm
 
         super().__init__(
@@ -40,20 +39,6 @@ class HDFStatsPimte(StandardDetector):
             config_sigs=config_sigs,
             name=name,
         )
-
-    @AsyncStatus.wrap
-    async def trigger(self) -> None:
-        """Arm the detector and wait for it to finish."""
-        indices_written = await self.writer.get_indices_written()
-        written_status = await self.controller.arm(1)
-        await written_status
-        end_observation = indices_written + 1
-
-        async for index in self.writer.observe_indices_written(
-            self._frame_writing_timeout
-        ):
-            if index >= end_observation:
-                break
 
     @property
     def hints(self) -> Hints:
--- src/i10_bluesky/plans/rasor/pimte_plans.py
+++ src/i10_bluesky/plans/rasor/pimte_plans.py
@@ -1,7 +1,18 @@
+import asyncio
+
 from bluesky import plan_stubs as bps
 from bluesky import preprocessors as bpp
-from bluesky.utils import Msg
+from bluesky.utils import Msg, short_uid
 from ophyd_async.core import DetectorTrigger, StandardDetector, TriggerInfo
+
+from i10_bluesky.devices.pimteAD import HDFStatsPimte
+from i10_bluesky.epics.drivers.pimte1_driver import Pimte1Driver
+
+"""
+    AdPlan store the state of an area detector and its associated functions.
+
+
+"""
 
 
 class AdPlan:
@@ -9,17 +20,35 @@ class AdPlan:
         self.exposure: float = 0.002
         self.trigger: DetectorTrigger = DetectorTrigger.internal
         self.n_img: int = 1
-        self.det: StandardDetector = det
+        self.det: HDFStatsPimte = det
         self.deadtime: float = self.det.controller.get_deadtime(self.exposure)
+
+    """
+        Bare min to take an image using prepare plan with full detector control
+    """
 
     def takeImg(
         self,
         exposure: float = None,
         n_img: int = None,
         det_trig: DetectorTrigger = None,
-    ) -> Msg:
+    ):
         self._updateDetInfo(exposure, n_img, det_trig)
-        return AdPlan.tiggerImg(self.det, self._getTriggerInfo())
+        grp = short_uid("prepare")
+
+        @bpp.stage_decorator([self.det])
+        @bpp.run_decorator()
+        def innerTakeImg():
+            yield from bps.declare_stream(self.det, name="primary")
+            yield from bps.kickoff(self.det, group=grp, wait=True)
+            yield from bps.prepare(self.det, self._getTriggerInfo, group=grp, wait=True)
+            # yield from bps.complete(self.det, group = grp, wait = True)
+            yield from bps.wait(
+                group=grp
+            )  # wait within the bps plan seem to have no effect hence wait here
+            return (yield from bps.complete(self.det, group=grp, wait=True))
+
+        return (yield from innerTakeImg())
 
     def _updateDetInfo(
         self,
@@ -37,6 +66,18 @@ class AdPlan:
 
     def _getTriggerInfo(self) -> TriggerInfo:
         return TriggerInfo(self.n_img, self.trigger, self.deadtime, self.exposure)
+
+    def setTemp(self, temp):
+        asyncio.create_task(self.det.controller.set_temperature(temp))
+        yield Msg("read", self.det.drv.read_backtemperture)
+
+    def setAdcSpeed(self, speed: Pimte1Driver.SpeedMode):
+        asyncio.create_task(self.det.controller.set_speed(speed))
+        yield Msg("read", self.det.drv.speed)
+
+    """
+        Static function for trigger with changable count time
+    """
 
     def tiggerImg(dets: StandardDetector, value: int):
         yield Msg("set", dets.drv.acquire_time, value)
