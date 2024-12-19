3447c3940c20bc9b04f4e77699962b12f73eeb29
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-12-19 14:17:05 +0000

    add p99 in pyprojet to use some of its plan

--- src/i10_bluesky/plans/utils/alignments.py
+++ src/i10_bluesky/plans/utils/alignments.py
@@ -7,6 +7,7 @@ from bluesky.plans import scan
 from dodal.common.types import MsgGenerator
 from ophyd_async.core import StandardReadable
 from ophyd_async.epics.motor import Motor
+from p99_bluesky.plans.fast_scan import fast_scan_1d
 
 
 class PeakPosition(int, Enum):
@@ -35,6 +36,38 @@ def find_peak_centre(
 
     yield from bpp.subs_wrapper(
         scan([det], motor, start, end, num=num),
+        ps,
+    )
+    return ps
+
+
+def fast_scan_and_move(
+    det: StandardReadable,
+    motor: Motor,
+    start: float,
+    end: float,
+    num: int,
+    loc: PeakPosition = PeakPosition.CEN,
+) -> MsgGenerator:
+    ps = yield from find_peak_centre_fast(
+        motor=motor, det=det, start=start, end=end, num=num
+    )
+    yield from abs_set(motor, ps["stats"][loc], wait=True)
+
+
+def find_peak_centre_fast(
+    det: StandardReadable,
+    motor: Motor,
+    start: float,
+    end: float,
+    motor_speed: float | None = None,
+) -> MsgGenerator:
+    ps = PeakStats(f"{motor.name}", f"{det.name}")
+
+    yield from bpp.subs_wrapper(
+        fast_scan_1d(
+            dets=[det], motor=motor, start=start, end=end, motor_speed=motor_speed
+        ),
         ps,
     )
     return ps
